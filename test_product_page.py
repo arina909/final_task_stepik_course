91b1d323e3c235cd385d0b7bf7899f92ab4502ca
arina909 <sverchkova.arina@yandex.ru>
2020-10-12 00:05:27 +0300

    add prametrised product page test

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,15 @@
 from .pages.product_page import ProductPage
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+product_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+xfail = 7
+urls = [product_link+"{no}" for no in range(10) if no!= xfail]
+xlink = pytest.param(product_link+str(xfail), marks=pytest.mark.xfail(reason="mistake on page"))
+urls.insert(xfail, xlink)
+
+@pytest.mark.parametrize('link', urls)
+
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()             
     page.add_product_to_basket()
